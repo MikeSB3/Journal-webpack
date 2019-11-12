73322d2c213ba6cc06623734f1af5e494f490d15
Student <example@example.com>
2019-11-12 11:51:58 -0800

    add journalEntry object, displayed results on DOM

--- src/main.js
+++ src/main.js
@@ -5,9 +5,16 @@ import './styles.css';
 
 $(document).ready(function() {
   $(".entryForm").submit(function(event) {
-    event.preventDefault();
     var titleInput = $('#title').val();
     var bodyInput = $('#body').val();
+    var journalEntry = new JournalEntry(titleInput, bodyInput);
+    event.preventDefault();
     console.log(titleInput, bodyInput);
+    console.log(journalEntry);
+    // console.log(journalEntry);
+    // console.log(journalEntry.title);
+    // console.log(Object.keys(journalEntry));
+    $('#journalEntry').html("<h4 id='journalTitle'>" + titleInput + "</h4><br><p id='journalBody'>" + bodyInput + "</p>");
+    // $('#journalBody').
   });
 });
